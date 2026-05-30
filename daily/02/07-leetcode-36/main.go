a8e2f3834b8e1376672f4e57f3807ae6d4fea13b
ngdangdat <ngdangdat09@gmail.com>
2026-05-30 19:00:50 +0900

    ✅ Valid Sudoku (LeetCode #36) - Grade: A

--- daily/02/07-leetcode-36/main.go
+++ daily/02/07-leetcode-36/main.go
@@ -7,39 +7,19 @@ func getBoxIndex(r, c int) int {
 }
 
 func isValidSudoku(board [][]byte) bool {
-	rowTrackingMap := make(map[int]map[rune]bool)
-	colTrackingMap := make(map[int]map[rune]bool)
-	boxTrackingMap := make(map[int]map[rune]bool)
-	for r := range 9 {
-		rowTrackingMap[r] = make(map[rune]bool)
-	}
-	for c := range 9 {
-		colTrackingMap[c] = make(map[rune]bool)
-	}
-
-	for r := range 3 {
-		for c := range 3 {
-			index := r*3 + c
-			boxTrackingMap[index] = make(map[rune]bool)
-		}
-	}
+	rowTrackingMap := [9][9]bool{}
+	colTrackingMap := [9][9]bool{}
+	boxTrackingMap := [9][9]bool{}
 
 	for r := range 9 {
 		for c := range 9 {
-			b := rune(board[r][c])
-			boxIndex := getBoxIndex(r, c)
-			// fmt.Printf("b=%q r=%d, c=%d, boxIndex=%d\n", b, r, c, boxIndex)
-			if b == '.' {
+			if rune(board[r][c]) == '.' {
 				continue
 			}
+			b := int(board[r][c]-'0') - 1
+			boxIndex := getBoxIndex(r, c)
 
 			if colTrackingMap[c][b] || rowTrackingMap[r][b] || boxTrackingMap[boxIndex][b] {
-				// fmt.Printf("c=%d r=%d boxIndex=%dcolTracking=%t rowTracking=%t boxTracking=%t\n",
-				// 	c, r, boxIndex,
-				// 	colTrackingMap[c][b],
-				// 	rowTrackingMap[r][b],
-				// 	boxTrackingMap[boxIndex][b],
-				// )
 				return false
 			}
 			colTrackingMap[c][b] = true
