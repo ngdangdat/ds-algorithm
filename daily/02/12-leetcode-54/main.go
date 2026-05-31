573b2b765e167e431ea23173841c93cad9d2bfce
ngdangdat <ngdangdat09@gmail.com>
2026-05-31 14:10:03 +0900

    ✅ Spiral Matrix (LeetCode #54) - Grade: A

--- daily/02/12-leetcode-54/main.go
+++ daily/02/12-leetcode-54/main.go
@@ -1,49 +1,37 @@
 package main
 
-func getNextVisit(numRow, numCol, r, c int) (int, int, bool) {
-	if r == 0 {
-		if c == numCol-1 {
-			return r + 1, c, false
-		}
-		return r, c + 1, false
-	}
-	if c == numCol-1 {
-		if r == numRow-1 {
-			return r, c - 1, false
-		}
-		return r + 1, c, false
-	}
-	if r == numRow-1 {
-		if c == 0 {
-			return r - 1, c, false
-		}
-		return r, c - 1, false
-	}
-	if r == 1 {
-		return 0, 0, true
-	}
-	return r - 1, c, false
-}
-
 func spiralOrder(matrix [][]int) []int {
-	visitedCount := 0
 	numRow := len(matrix)
 	numCol := len(matrix[0])
 	total := numRow * numCol
-	buffer, r, c := 0, 0, 0
 	res := make([]int, total)
-	isEnd := false
-	for visitedCount < total {
-		res[visitedCount] = matrix[r][c]
-		r, c, isEnd = getNextVisit(numRow, numCol, r-buffer, c-buffer)
-		if isEnd {
-			numRow -= 2
-			numCol -= 2
-			buffer += 1
+	top, bottom, right, left := 0, numRow-1, numCol-1, 0
+	index := 0
+	for (left <= right) && (top <= bottom) {
+		for c := left; c <= right; c++ {
+			res[index] = matrix[top][c]
+			index++
 		}
-		r += buffer
-		c += buffer
-		visitedCount += 1
+		top++
+		for r := top; r <= bottom; r++ {
+			res[index] = matrix[r][right]
+			index++
+		}
+		right--
+		if top <= bottom {
+			for c := right; c >= left; c-- {
+				res[index] = matrix[bottom][c]
+				index++
+			}
+			bottom--
+		}
+		if left <= right {
+			for r := bottom; r >= top; r-- {
+				res[index] = matrix[r][left]
+				index++
+			}
+			left++
+		}
 	}
 	return res
 }
