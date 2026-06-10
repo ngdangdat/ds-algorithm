769b67dac97771860b3924ac58e610d1fb427403
ngdangdat <ngdangdat09@gmail.com>
2026-06-11 00:17:50 +0900

    fix: Remove Duplicates from Sorted Array (LeetCode #26) - Grade: C → A

--- daily/02/21-leetcode-26/main.go
+++ daily/02/21-leetcode-26/main.go
@@ -6,17 +6,9 @@ func removeDuplicates(nums []int) int {
 	}
 	l, r := 0, 1
 	for r < len(nums) {
-		for r < len(nums)-1 && nums[r] == nums[l] {
-			r++
-		}
-		if r < len(nums) && nums[r] != nums[l] {
-			if l < r-1 {
-				// swap needed here
-				l++
-				nums[l] = nums[r]
-			} else if l == r-1 {
-				l++
-			}
+		if nums[r] != nums[l] {
+			nums[l+1] = nums[r]
+			l++
 		}
 		r++
 	}
